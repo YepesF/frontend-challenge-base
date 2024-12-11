5975aa09ec69ad1dec27befccdb0481fbc714034
YepileF <yepilef@gmail.com>
2024-12-11 11:40:44 -0500

    fix: serve actions

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,34 +1,6 @@
-import { IResponseTMDB } from "@/interfaces/TMDB";
+import { getMovies } from "@/actions/movie";
 
 export const revalidate = 86400;
-
-const getMovies = async (page: string = "1"): Promise<IResponseTMDB> => {
-  try {
-    const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`;
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
-      },
-    };
-
-    const res = await fetch(url, options);
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
-    }
-
-    const json: IResponseTMDB = await res.json();
-    return json;
-  } catch (error) {
-    return {
-      results: [],
-      total_pages: 0,
-      total_results: 0,
-      page: 0,
-    } as IResponseTMDB;
-  }
-};
 
 export async function generateStaticParams(): Promise<{ id: string }[]> {
   const maxPages = 500;
