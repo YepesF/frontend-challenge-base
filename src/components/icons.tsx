5f8749c952af853f99ee89b4241f01cb2395a45d
YepileF <yepilef@gmail.com>
2024-12-11 22:01:59 -0500

    add: chevron-left icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -194,3 +194,66 @@ export const MagnifyingGlassIcon = ({
       );
   }
 };
+
+export const ChevronLeftIcon = ({
+  type,
+  className,
+  ...props
+}: IIconProps): JSX.Element => {
+  switch (type) {
+    case SvgType.OUTLINE:
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={cn("size-6", className)}
+          {...props}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
+        </svg>
+      );
+
+    case SvgType.SOLID:
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className={cn("size-6", className)}
+          {...props}
+        >
+          <path
+            fillRule="evenodd"
+            d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      );
+
+    default:
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={cn("size-6", className)}
+          {...props}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
+        </svg>
+      );
+  }
+};
