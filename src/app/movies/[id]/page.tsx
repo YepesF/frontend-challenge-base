3ebff5d2947e568e60c150abdfaf49d63178b763
YepileF <yepilef@gmail.com>
2024-12-11 22:26:23 -0500

    feat: hero movie page

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { getMovieById, getMovies } from "@/actions/movie";
+import FavoriteButton from "@/components/favorite-button";
+import { ChevronLeftIcon } from "@/components/icons";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 86400;
 
@@ -26,43 +30,53 @@ export default async function MoviePage({
   const movieId = parseInt(params.id);
   const movie = await getMovieById(movieId);
   return (
-    <div className="aspect-video shadow-xl lg:aspect-[3/1]">
-      <div className="relative h-full w-full">
-        <Image
-          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-          alt="backdrop"
-          fill
-          className="brightness-[.35]"
+    <div>
+      <div className="relative flex h-[50vh] items-center justify-center shadow-xl">
+        <Link
+          href="/"
+          className="absolute left-5 top-5 z-50 w-min cursor-pointer rounded-full bg-secondary p-2 text-white transition hover:scale-[1.03]"
+        >
+          <ChevronLeftIcon type="outline" />
+        </Link>
+        <FavoriteButton
+          variant="outline"
+          className="absolute right-6 top-6 z-50"
         />
-        <div className="absolute top-0 grid h-full w-full grid-cols-12 gap-4 px-28 py-5">
-          <div className="col-span-3 flex flex-col gap-3 p-2">
-            <div className="relative aspect-[4/5]">
-              <Image
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt="poster"
-                fill
-              />
+        <Image
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          alt="banner"
+          fill
+          style={{
+            filter: "drop-shadow(0 0 5px rgba(0, 0, 0, 5))",
+            maskImage: "linear-gradient(black 30%, transparent)",
+          }}
+        />
+        <div className="absolute bottom-10 left-5 z-50 flex w-fit flex-col items-start justify-start">
+          <div className="flex items-center justify-start gap-2">
+            <div className="relative h-10 w-10">
+              <Image src="/imdb.png" alt="imdb" fill />
             </div>
-            <Button className="bg-yellow-500 font-bold text-black transition hover:scale-[1.03] hover:bg-yellow-500/90">
-              Watch trailer
-            </Button>
+            <span className="w-min font-semibold text-white">
+              {movie.vote_average.toFixed(1)}
+            </span>
           </div>
-          <div className="col-span-8 flex flex-col gap-10 p-2">
-            <div className="w-fit">
-              <h1 className="text-start text-4xl font-bold text-white">
-                {movie.title}
-              </h1>
-              <ul className="flex gap-4 text-white">
-                <li>{movie.release_date}</li>
-                <li className="text-end">{movie.runtime} Min.</li>
-              </ul>
+          <h1 className="w-fit text-2xl font-bold text-white">{movie.title}</h1>
+          <div className="flex w-96 flex-col items-start justify-between gap-4">
+            <div className="flex gap-2 py-2">
+              {movie.genres.slice(0, 4).map(({ name }) => (
+                <Badge
+                  className="w-fit rounded-full border-accent bg-accent/15 text-accent"
+                  variant="outline"
+                >
+                  {name}
+                </Badge>
+              ))}
             </div>
-
-            <h2 className="w-fit text-start text-2xl font-bold text-white">
-              Overview:
-            </h2>
-
-            <p className="text-start text-xl text-white">{movie.overview}</p>
+            <Link href={`/movies/${movie.id}`} className="w-fit">
+              <Button className="w-40 rounded-full bg-accent font-bold text-white transition hover:scale-[1.03] hover:bg-accent/95">
+                Trailer
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
