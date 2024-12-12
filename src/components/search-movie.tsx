1fa75bbc15620b13fd347390b0d27a8777387584
YepileF <yepilef@gmail.com>
2024-12-11 21:31:57 -0500

    refactor: ssearch movie

--- src/components/search-movie.tsx
+++ src/components/search-movie.tsx
@@ -1,47 +1,89 @@
 "use client";
 
-import useParams from "@/hooks/useParams";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import { Button } from "./ui/button";
+import { useState } from "react";
+import { MagnifyingGlassIcon } from "./icons";
 import { Input } from "./ui/input";
-import { FormEvent, useState } from "react";
-import { SheetClose } from "./ui/sheet";
+import useParams from "@/hooks/useParams";
 
-export default function SearchMovie(): JSX.Element {
+interface ISearchMovieProps {
+  children: React.ReactNode;
+}
+
+export default function SearchMovie({
+  children,
+}: ISearchMovieProps): JSX.Element {
   const { updateParam } = useParams();
   const [query, setQuery] = useState<string>("");
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     updateParam("query", query);
-    updateParam("page", "1");
+    setQuery("");
   };
 
   return (
-    <form className="flex gap-2" onSubmit={handleSubmit}>
-      <Input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <SheetClose asChild>
-        <Button variant="outline" size="icon" type="submit">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="rounded-lg border p-0 shadow-md md:min-w-[450px]">
+        <DialogHeader className="hidden">
+          <DialogTitle></DialogTitle>
+        </DialogHeader>
+        <form className="mb-0" onSubmit={handleSubmit}>
+          <div className="relative flex items-center">
+            <MagnifyingGlassIcon
+              type="outline"
+              className="absolute left-2 size-5"
             />
-          </svg>
-        </Button>
-      </SheetClose>
-    </form>
+            <Input
+              type="text"
+              placeholder="Type your movie and press Enter"
+              className="pl-8"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
+          <DialogClose asChild>
+            <Button
+              className="hidden"
+              type="submit"
+              variant="ghost"
+              size="icon"
+            >
+              Close
+            </Button>
+          </DialogClose>
+        </form>
+        <Command className="">
+          <CommandList>
+            <CommandGroup heading="Recent searches">
+              <CommandItem>
+                <span>Calendar</span>
+              </CommandItem>
+              <CommandItem>
+                <span>Search Emoji</span>
+              </CommandItem>
+              <CommandItem disabled>
+                <span>Calculator</span>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </DialogContent>
+    </Dialog>
   );
 }
