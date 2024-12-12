2bf68952be9c66645de7b270ce25e77b99710055
YepileF <yepilef@gmail.com>
2024-12-11 21:32:30 -0500

    add: search component in navbar

--- src/components/mobile-nav-bar.tsx
+++ src/components/mobile-nav-bar.tsx
@@ -4,11 +4,12 @@ import { usePathname } from "next/navigation";
 import { BookmarkIcon, HomeIcon, MagnifyingGlassIcon } from "./icons";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import SearchMovie from "./search-movie";
 
 export default function MobileNavBar(): JSX.Element {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 z-10 flex h-24 w-full items-start justify-center gap-20 bg-secondary/80 py-1 pt-3 backdrop-blur-sm backdrop-filter">
+    <div className="fixed bottom-0 z-50 flex h-24 w-full items-start justify-center gap-20 bg-secondary/80 py-1 pt-3 backdrop-blur-sm backdrop-filter">
       <Link
         href="/"
         className={cn(
@@ -22,10 +23,12 @@ export default function MobileNavBar(): JSX.Element {
         />
         <span className="text-center text-sm font-medium">Home</span>
       </Link>
-      <div className="flex w-fit cursor-pointer flex-col items-center justify-center text-white transition hover:scale-[1.03] hover:!text-accent">
-        <MagnifyingGlassIcon type="outline" className="h-10 w-10" />
-        <span className="text-center text-sm font-medium">Explore</span>
-      </div>
+      <SearchMovie>
+        <div className="flex w-fit cursor-pointer flex-col items-center justify-center text-white transition hover:scale-[1.03] hover:!text-accent">
+          <MagnifyingGlassIcon type="outline" className="h-10 w-10" />
+          <span className="text-center text-sm font-medium">Explore</span>
+        </div>
+      </SearchMovie>
       <Link
         href="/favorites"
         className={cn(
